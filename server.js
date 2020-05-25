5b8e87ba7a1af4e03f2d514b59688cd3cd16ff29
rayar <rayarao.anand@gmail.com>
2020-05-25 11:11:47 +0530

    d

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/my-new-angular-app'));
 
-app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')) ) ;
-
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/my-new-angular-app/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT);//Install express server
+app.listen(process.env.PORT || 8080);//Install express server
