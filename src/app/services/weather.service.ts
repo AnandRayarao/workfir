b7ad7c2287129092edbbcd403da04313371382fa
rayar <rayarao.anand@gmail.com>
2020-05-25 12:38:48 +0530

    d

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -11,7 +11,7 @@ export class WeatherService {
   get_weather(city){
 
      this.http.get(
-      'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=e0891b6be00f418dd2785f1e5760d890&units=imperial'
+      'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=e0891b6be00f418dd2785f1e5760d890&units=imperial'
   ).subscribe((data) => {
     this.x[0] = data
 });
